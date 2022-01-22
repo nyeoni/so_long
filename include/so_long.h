eb38813ddcde02ffdd8262c1c38e2831ff2c95ea
Chloekkk <skdusdl8804@gmail.com>
2022-01-22 18:23:13 +0900

    update : so_long lib setting

--- include/so_long.h
+++ include/so_long.h
@@ -6,14 +6,63 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 20:55:51 by nkim              #+#    #+#             */
-/*   Updated: 2022/01/18 21:01:15 by nkim             ###   ########.fr       */
+/*   Updated: 2022/01/22 18:19:29 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
+// Common Lib
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+// My Lib
 #include "../lib/libft42/include/libft42.h"
 #include "../lib/mlx/mlx.h"
+
+#define TRUE 1
+#define FALSE 0
+
+#define X_EVNET_KEY_EXIT 17
+
+// Structures
+typedef struct s_map {
+	char **map;
+	int rows;
+	int cols;
+	int p_cnt;
+	int c_cnt;
+	int e_cnt;
+}	t_map;
+
+typedef struct s_game
+{
+	void *mlx_ptr;
+	void *win_ptr;
+	t_map map;
+
+} t_game;
+
+// Directory functions
+// ERROR DIR
+void throwError(char *message);
+
+// MAP DIR
+// parse_map.c
+void parseMap(t_game *game, char *fileName);
+
+// validate_map.c
+void extensionValidate(char *fileName);
+void charactersValidate(char *line);
+void lengthValidate(char *src, int length);
+void wallValidate(t_map *map, int row_idx);
+void leastValidate(t_map *map);
+
+// UTILS DIR
+// FT_POINTER_REALLOC.C
+void *ft_ptr_realloc(void *buf, int before_size, int after_size);
+char *get_line(int fd);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,41 +6,37 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 18:56:18 by nkim              #+#    #+#             */
-/*   Updated: 2022/01/20 00:29:18 by nkim             ###   ########.fr       */
+/*   Updated: 2022/01/22 18:19:51 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "mlx.h"
+#include "so_long.h"
 
-typedef struct s_manager {
-	void *mlx_ptr;
-	void *win_ptr;
-} t_manager;
-
-int	close_game(t_manager *manager)
+int	close_game(t_game *game)
 {
 	printf("close_game\n");
-	mlx_destroy_window(manager->mlx_ptr, manager->win_ptr);
-	exit(0);
+	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+
+	exit(EXIT_SUCCESS);
 }
 
-int	main()
+int	main(int argc, char **argv)
 {
-	t_manager manager;
-	int X_EVNET_KEY_EXIT = 17;
+	t_game game;
 	int width;
 	int height;
-
 	void *img;
 
-	manager.mlx_ptr = mlx_init(); // connection identifier 생성
-	manager.win_ptr = mlx_new_window(manager.mlx_ptr, 500, 500, "Chloe's Project"); // window identifier 생성
-	mlx_pixel_put(manager.win_ptr, manager.win_ptr, 0, 0, 0x00FF0000);
-	img = mlx_xpm_file_to_image(manager.mlx_ptr,"./asset/tile.xpm", &width, &height);
-	mlx_put_image_to_window(manager.mlx_ptr, manager.win_ptr, img, 0, 0);
-	mlx_hook(manager.win_ptr, X_EVNET_KEY_EXIT, 0, &close_game, &manager);
-	mlx_loop(manager.mlx_ptr);
+	if (argc != 2)
+		throwError("ArgError : no / over argument!");
+	parseMap(&game, argv[1]);
+
+	game.mlx_ptr = mlx_init();												  // connection identifier 생성
+	game.win_ptr = mlx_new_window(game.mlx_ptr, 500, 500, "Chloe's Project"); // window identifier 생성
+	mlx_pixel_put(game.win_ptr, game.win_ptr, 0, 0, 0x00FF0000);
+	img = mlx_xpm_file_to_image(game.mlx_ptr,"./asset/tile.xpm", &width, &height);
+	mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, img, 0, 0);
+	mlx_hook(game.win_ptr, X_EVNET_KEY_EXIT, 0, &close_game, &game);
+	mlx_loop(game.mlx_ptr);
 	return (0);
 }
