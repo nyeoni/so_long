1a2fc8ca271221aeb79a572113129827cc090f03
Chloekkk <skdusdl8804@gmail.com>
2022-01-24 16:00:03 +0900

    update : add wall, ground, exit

--- src/game/init_game.c
+++ src/game/init_game.c
@@ -6,11 +6,40 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 22:18:35 by nkim              #+#    #+#             */
-/*   Updated: 2022/01/23 19:05:01 by nkim             ###   ########.fr       */
+/*   Updated: 2022/01/24 15:58:39 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void ft_make_iterable_sprites(t_game *game, t_sprites **sprites, char *imgName, int cnt) {
+	t_sprites *tmp;
+	char *fileName;
+	char *xpmFile;
+	int num;
+	int idx;
+
+	*sprites = (t_sprites *)malloc(sizeof(t_sprites));
+	tmp = *sprites;
+	idx = 0;
+	while (idx < cnt) {
+		num = '0' + idx;
+		fileName = ft_strjoin(imgName, (char *)&num);
+		xpmFile = ft_strjoin(fileName, ".xpm");
+		tmp->img = ft_make_xpm_img(game, xpmFile);
+		printf("file: %s\n", xpmFile);
+		printf("%p\n", tmp->img);
+		free(fileName);
+		free(xpmFile);
+		if (idx != cnt - 1)
+		{
+			tmp->next = (t_sprites *)malloc(sizeof(t_sprites));
+			tmp = tmp->next;
+		}
+		idx++;
+	}
+	tmp->next = *sprites;
+}
 
 void draw_tiles(t_game *game) {
 	t_map map;
@@ -28,15 +57,50 @@ void draw_tiles(t_game *game) {
 		{
 			if (line[col_idx] == '1')
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
-						game->tiles.wall, col_idx * TILE_SIZE, row_idx * TILE_SIZE);
+				game->tiles.wall, col_idx * TILE_SIZE, row_idx * TILE_SIZE);
+			else if (line[col_idx] == 'E')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
+				game->tiles.exit, col_idx * TILE_SIZE, row_idx * TILE_SIZE);
 			else
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
-						game->tiles.ground, col_idx * TILE_SIZE, row_idx * TILE_SIZE);
+				game->tiles.ground, col_idx * TILE_SIZE, row_idx * TILE_SIZE);
+			col_idx++;
+		}
+		row_idx++;
+	}
+	// backup basic win_ptr
+	game->win_bak_ptr = game->win_ptr;
+}
+
+void draw_sprites(t_game *game) {
+	t_map map;
+	char *line;
+	int row_idx;
+	int col_idx;
+
+	map = game->map;
+	row_idx = 0;
+	while (row_idx < map.rows)
+	{
+		line = map.map[row_idx];
+		col_idx = 0;
+		while (col_idx < map.cols)
+		{
+			if (line[col_idx] == 'P'){
+				game->player.x = col_idx * TILE_SIZE;
+				game->player.y = row_idx * TILE_SIZE;
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
+				game->player.sprites->img, game->player.x, game->player.y);
+			}
+			else if (line[col_idx] == 'C')
+				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
+				game->collect->img, col_idx * TILE_SIZE, row_idx * TILE_SIZE);
 			col_idx++;
 		}
 		row_idx++;
 	}
 }
+
 
 void init_window(t_game *game) {
 	int x;
@@ -45,21 +109,30 @@ void init_window(t_game *game) {
 	x = game->map.cols;
 	y = game->map.rows;
 	game->mlx_ptr = mlx_init();
-	game->win_ptr = mlx_new_window(game->mlx_ptr, TILE_SIZE * x, TILE_SIZE * y, "nkim's so_long");
+	game->win_ptr = mlx_new_window(game->mlx_ptr, TILE_SIZE * x, TILE_SIZE * y, "KIRBY");
 }
 
-void init_tiles(t_game *game) {
+void init_imgs(t_game *game) {
 	t_tiles *tiles;
+	t_player *player;
 
 	tiles = &game->tiles;
+	player = &game->player;
 	tiles->wall = ft_make_xpm_img(game, "wall.xpm");
 	tiles->ground = ft_make_xpm_img(game, "ground.xpm");
+	tiles->exit = ft_make_xpm_img(game, "exit.xpm");
+
+	ft_make_iterable_sprites(game, &player->sprites, PLAYER_NAME, 9);
+	ft_make_iterable_sprites(game, &game->collect, "star", 2);
 }
 
 void init_game(t_game *game) {
 	init_window(game);
-	init_tiles(game);
+	init_imgs(game);
 	draw_tiles(game);
+	draw_sprites(game);
+
+	printf("%p\n", game->player.sprites->img);
 }
 
 void start_game(t_game *game) {
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 18:56:18 by nkim              #+#    #+#             */
-/*   Updated: 2022/01/23 18:57:15 by nkim             ###   ########.fr       */
+/*   Updated: 2022/01/24 15:06:18 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,27 @@ int	close_game(t_game *game)
 	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
 
 	exit(EXIT_SUCCESS);
+}
+
+int walk_player(t_game *game)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (i < 10) {
+		while (j < 10) {
+			// mlx_put_image_to_window(game->mlx_ptr, game->win_ptr,
+			// 						void *img_ptr, int x, int y);
+			mlx_put_image_to_window(game->mlx_ptr, game->win_bak_ptr,
+									game->player.sprites->img, game->player.x * 6, game->player.y);
+			j++;
+		}
+		i++;
+	}
+	game->player.sprites = game->player.sprites->next;
+	return 1;
 }
 
 int	main(int argc, char **argv)
@@ -36,6 +57,7 @@ int	main(int argc, char **argv)
 	// mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, img, 0, 0);
 	// mlx_put_image_to_window(game.mlx_ptr, game.win_ptr, img, 64, 0);
 	mlx_hook(game.win_ptr, X_EVNET_KEY_EXIT, 0, &close_game, &game);
+	mlx_loop_hook(game.mlx_ptr, &walk_player, &game);
 	mlx_loop(game.mlx_ptr);
 	return (0);
 }
