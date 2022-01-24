f0a981305be43c8d9c0a83e886924a29dac30a82
Chloekkk <skdusdl8804@gmail.com>
2022-01-24 15:59:39 +0900

    update : ft_make_xpm_img.c

--- src/utils/ft_make_xpm_img.c
+++ src/utils/ft_make_xpm_img.c
@@ -6,23 +6,25 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:04:18 by nkim              #+#    #+#             */
-/*   Updated: 2022/01/23 19:04:34 by nkim             ###   ########.fr       */
+/*   Updated: 2022/01/23 23:40:55 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void *ft_make_xpm_img(t_game *game, char *fileName) {
+void *ft_make_xpm_img(t_game *game, char *xpmFile) {
 	char *path;
 	void *img;
 	int width;
 	int height;
 
-	path = ft_strjoin(ASSET_PATH, fileName);
+	path = ft_strjoin(ASSET_PATH, xpmFile);
 	img = mlx_xpm_file_to_image(game->mlx_ptr, path, &width, &height);
 	free(path);
-	if (!img)
+	if (!img){
+		printf("fileName: %s\n", xpmFile);
 		throw_error("XPM Error : check ASSET_PATH or fileName");
+	}
 	else if (!(width == TILE_SIZE && height == TILE_SIZE))
 		throw_error("TILE SIZE Error : incorrect tile size");
 
