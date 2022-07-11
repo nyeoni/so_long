98c120ea084976eeaf1397722c8a6d67b58c47df
Chloekkk <skdusdl8804@gmail.com>
2022-07-12 08:21:44 +0900

    feat: Update screen xpm_image convertor

--- src_bonus/utils_bonus/ft_make_xpm_img_bonus.c
+++ src_bonus/utils_bonus/ft_make_xpm_img_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 19:04:18 by nkim              #+#    #+#             */
-/*   Updated: 2022/07/12 04:26:15 by nkim             ###   ########.fr       */
+/*   Updated: 2022/07/12 06:19:12 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,26 @@ void	*ft_make_xpm_img(t_game *game, char *xpmFile)
 		throw_error("XPM Error : check ASSET_PATH or fileName");
 	}
 	else if (!(width == TILE_SIZE && height == TILE_SIZE))
+		throw_error("TILE SIZE Error : incorrect tile size");
+	return (img);
+}
+
+void	*ft_make_xpm_screen(t_game *game, char *xpmFile)
+{
+	char	*path;
+	void	*img;
+	int		width;
+	int		height;
+
+	path = ft_strjoin(ASSET_PATH, xpmFile);
+	img = mlx_xpm_file_to_image(game->mlx_ptr, path, &width, &height);
+	free(path);
+	if (!img)
+	{
+		printf("fileName: %s\n", xpmFile);
+		throw_error("XPM Error : check ASSET_PATH or fileName");
+	}
+	else if (!(width == SCREEN_WIDTH && height == SCREEN_HEIGHT))
 		throw_error("TILE SIZE Error : incorrect tile size");
 	return (img);
 }
